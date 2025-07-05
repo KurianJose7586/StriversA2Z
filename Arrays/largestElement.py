2edad7a417f146443d62dea754893b3031ebb0a4
Kurian Jose <140304094+KurianJose7586@users.noreply.github.com>
2025-07-05 17:01:49 +0530

    Update largestElement.py

--- Arrays/largestElement.py
+++ Arrays/largestElement.py
@@ -1,1 +1,11 @@
-print("hello")
+def findLargest(arr):
+  max = arr[0]
+  for i in arr:
+    if i > max:
+      max = i
+    else:
+      continue
+  return max
+
+findLargest([3,4,7,1,2,10,0,1])
+  
